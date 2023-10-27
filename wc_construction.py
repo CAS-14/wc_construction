d35fdb0563621df9a17557a67f63d11854585985
Charles <78571139+CAS-14@users.noreply.github.com>
2023-10-27 07:42:24 -0400

    update domain

--- wc_construction.py
+++ wc_construction.py
@@ -2,7 +2,7 @@ from flask import request
 
 import tools
 
-bp = tools.MyBlueprint("cons", "wc_construction", host="weirdcease.com")
+bp = tools.MyBlueprint("cons", "wc_construction", host="cas.act25.com")
 
 @bp.route("/")
 @bp.route("/<anything>")
