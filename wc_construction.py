de7a9bc57b0e67503f24e33553ffc5941ef0e487
CAS <78571139+cas-14@users.noreply.github.com>
2023-10-26 21:51:17 -0400

    anything2

--- wc_construction.py
+++ wc_construction.py
@@ -6,5 +6,5 @@ bp = tools.MyBlueprint("cons", "wc_construction", host="weirdcease.com")
 
 @bp.route("/")
 @bp.route("/<anything>")
-def home(anything):
+def home(anything: None):
     return bp.render("construction.html")
